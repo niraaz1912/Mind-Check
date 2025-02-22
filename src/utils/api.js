314c7c22bdddb870964d3cea06646aac753e0736
Huy Truong <hoang.truonghuy2004@gmail.com>
2025-02-21 20:38:04 -0600

    fixes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 
 export async function postFeelingInfo(inputValue){
-    const res = await fetch('/classify', {method:"post", body: JSON.stringify(inputValue)})
+    const res = await fetch("http://localhost:5000/classify", {method:"post", body: JSON.stringify({text: inputValue})})
     if(!res.ok){
         throw {
             message: "Failed",
