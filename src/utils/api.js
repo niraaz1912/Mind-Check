a06470faa54216a94f92fa9199a2b7c750c816f9
Huy Truong <hoang.truonghuy2004@gmail.com>
2025-02-21 21:01:25 -0600

    Add header to post request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,12 @@
 
 export async function postFeelingInfo(inputValue){
-    const res = await fetch("http://localhost:5000/classify", {method:"post", body: JSON.stringify({text: inputValue})})
+    const res = await fetch("http://127.0.0.1:5000/classify", {
+    method:"post",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    //mode: 'no-cors',  
+    body: JSON.stringify({text: inputValue})})
     if(!res.ok){
         throw {
             message: "Failed",
